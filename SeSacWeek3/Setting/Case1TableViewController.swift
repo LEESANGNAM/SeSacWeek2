76d29672343e1958ac35460a61822e6b600c29fa
LEESANGNAM <fgh0116@naver.com>
2023-07-27 21:12:15 +0900

    case1

---
+++ SeSacWeek3/Setting/Case1TableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Case1TableViewController.swift
+//  SeSacWeek3
+//
+//  Created by 이상남 on 2023/07/27.
+//
+
+import UIKit
+
+class Case1TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+
+}
