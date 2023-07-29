a77d620c356b5458c978e8688e1640b77721845c
LEESANGNAM <fgh0116@naver.com>
2023-07-29 15:20:24 +0900

    Movie 구조체 추가

---
+++ SeSacWeek3/Movie/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  SeSacWeek3
+//
+//  Created by 이상남 on 2023/07/29.
+//
+
+import UIKit
+
+struct Movie {
+    var title: String
+    var releaseDate: String
+    var runtime: Int
+    var overview: String
+    var rate: Double
+}
